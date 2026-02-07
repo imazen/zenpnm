6040e85443ecd7e661281e8453849bc5e12e0ffd
Lilith River <lilith@imazen.io>
2026-02-06 23:46:21 -0700

    fix: add .gitignore, remove target/ from tracking

--- target/debug/deps/enough-c8701152c4398686.d
+++
@@ -1,8 +0,0 @@
-/home/lilith/work/zenpnm/target/debug/deps/enough-c8701152c4398686.d: /home/lilith/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/enough-0.3.1/src/lib.rs /home/lilith/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/enough-0.3.1/src/reason.rs
-
-/home/lilith/work/zenpnm/target/debug/deps/libenough-c8701152c4398686.rlib: /home/lilith/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/enough-0.3.1/src/lib.rs /home/lilith/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/enough-0.3.1/src/reason.rs
-
-/home/lilith/work/zenpnm/target/debug/deps/libenough-c8701152c4398686.rmeta: /home/lilith/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/enough-0.3.1/src/lib.rs /home/lilith/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/enough-0.3.1/src/reason.rs
-
-/home/lilith/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/enough-0.3.1/src/lib.rs:
-/home/lilith/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/enough-0.3.1/src/reason.rs:
